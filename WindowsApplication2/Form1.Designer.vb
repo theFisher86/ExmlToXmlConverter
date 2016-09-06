cf9478a991d7e06bc5cfe7ea56ca07bead31bd56
Aaron Fisher <aaron@fishcodesign.com>
2016-09-06 18:31:10 -0400

    initial commit

---
+++ WindowsApplication2/Form1.Designer.vb
@@ -0,0 +1,82 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dlgExmlToXml = New System.Windows.Forms.OpenFileDialog()
+        Me.btnExmlToXml = New System.Windows.Forms.Button()
+        Me.btnXmlToExml = New System.Windows.Forms.Button()
+        Me.dlgXmlToExml = New System.Windows.Forms.OpenFileDialog()
+        Me.SuspendLayout()
+        '
+        'dlgExmlToXml
+        '
+        Me.dlgExmlToXml.AddExtension = False
+        Me.dlgExmlToXml.FileName = "ExmlFile"
+        Me.dlgExmlToXml.Filter = "EXML Files|*.exml|All Files|*.*"
+        Me.dlgExmlToXml.RestoreDirectory = True
+        Me.dlgExmlToXml.Title = "Choose exml File"
+        '
+        'btnExmlToXml
+        '
+        Me.btnExmlToXml.Location = New System.Drawing.Point(12, 12)
+        Me.btnExmlToXml.Name = "btnExmlToXml"
+        Me.btnExmlToXml.Size = New System.Drawing.Size(100, 23)
+        Me.btnExmlToXml.TabIndex = 0
+        Me.btnExmlToXml.Text = "exml -> xml"
+        Me.btnExmlToXml.UseVisualStyleBackColor = True
+        '
+        'btnXmlToExml
+        '
+        Me.btnXmlToExml.Location = New System.Drawing.Point(125, 12)
+        Me.btnXmlToExml.Name = "btnXmlToExml"
+        Me.btnXmlToExml.Size = New System.Drawing.Size(100, 23)
+        Me.btnXmlToExml.TabIndex = 1
+        Me.btnXmlToExml.Text = "xml -> exml"
+        Me.btnXmlToExml.UseVisualStyleBackColor = True
+        '
+        'dlgXmlToExml
+        '
+        Me.dlgXmlToExml.AddExtension = False
+        Me.dlgXmlToExml.FileName = "XMLFile"
+        Me.dlgXmlToExml.Filter = "XML Files|*.xml|All Files|*.*"
+        Me.dlgXmlToExml.RestoreDirectory = True
+        Me.dlgXmlToExml.Title = "Choose XML File"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(237, 44)
+        Me.Controls.Add(Me.btnXmlToExml)
+        Me.Controls.Add(Me.btnExmlToXml)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents dlgExmlToXml As OpenFileDialog
+    Friend WithEvents btnExmlToXml As Button
+    Friend WithEvents btnXmlToExml As Button
+    Friend WithEvents dlgXmlToExml As OpenFileDialog
+End Class
---
+++ WindowsApplication2/Form1.vb
@@ -0,0 +1,92 @@
+﻿Public Class Form1
+    Private Sub btnChooseFile_Click(sender As Object, e As EventArgs) Handles btnExmlToXml.Click
+        dlgExmlToXml.ShowDialog()
+    End Sub
+
+    Private Sub btnXmlToExml_Click(sender As Object, e As EventArgs) Handles btnXmlToExml.Click
+        dlgXmlToExml.ShowDialog()
+    End Sub
+
+    Private Sub dlgExmlFile_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles dlgExmlToXml.FileOk
+        Dim RawExmlString As String 
+        Dim ParsedXML As New XDocument
+        RawExmlString = My.Computer.FileSystem.ReadAllText(dlgExmlToXml.FileName)
+        ParsedXML = ParseExmlString(RawExmlString)
+        ParsedXML.Save(dlgExmlToXml.FileName & ".xml")
+    End Sub
+
+    Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles dlgXmlToExml.FileOk
+        Dim XMLString As String
+        Dim RevertedExml As New XDocument
+        XMLString = My.Computer.FileSystem.ReadAllText(dlgXmlToExml.FileName)
+        RevertedExml = RevertXmltoExml(XMLString)
+        If dlgXmlToExml.FileName.EndsWith(".exml.xml") Then
+            Dim NewFileName As String
+            NewFileName = dlgXmlToExml.FileName.Remove(dlgXmlToExml.FileName.Length - 4, 4)
+            RevertedExml.Save(NewFileName)
+        Else
+            RevertedExml.Save(dlgXmlToExml.FileName)
+        End If
+    End Sub
+
+    Public Function ParseExmlString(RawExmlString As String)
+        Dim xdoc As New XDocument()
+        'Dim ParsedXML As String
+        xdoc = XDocument.Parse(RawExmlString)
+        For Each element As XElement In xdoc.Descendants
+            Debug.Print(element.FirstAttribute)
+            If (element.Attribute("template") IsNot Nothing) Then
+                If (element.Attribute("name") Is Nothing) Then
+                    element.Name = "name_root"
+                Else
+                    element.Name = "name_" & element.Attribute("name").Value
+                    element.Attribute("name").Remove()
+                End If
+            Else
+                If element.Attribute("name") IsNot Nothing Then
+                    element.Name = "name_" & element.Attribute("name").Value
+                    element.Attribute("name").Remove()
+                    If (element.Attribute("value") IsNot Nothing) Then
+                        element.Value = element.Attribute("value").Value
+                        element.Attribute("value").Remove()
+                    End If
+                End If
+            End If
+        Next
+        'ParsedXML = xdoc.ToString
+        'Return ParsedXML
+        Return xdoc
+    End Function
+
+    Public Function RevertXmltoExml(XMLString As String)
+        Dim xdoc As New XDocument()
+        xdoc = XDocument.Parse(RevertXmltoExml)
+        For Each element As XElement In xdoc.Descendants
+            Debug.Print(element.FirstAttribute)
+            If (element.Attribute("template") IsNot Nothing) Then
+                If (element.Name = "name_root") Then
+                    element.Attribute("name") = Nothing
+                Else
+                    If (element.Name Like "name_*" Then
+
+
+                        element.Name = "name_" & element.Attribute("name").Value
+                        element.Attribute("name").Remove()
+                    End If
+                    Else
+                If element.Attribute("name") IsNot Nothing Then
+                    element.Name = "name_" & element.Attribute("name").Value
+                    element.Attribute("name").Remove()
+                    If (element.Attribute("value") IsNot Nothing) Then
+                        element.Value = element.Attribute("value").Value
+                        element.Attribute("value").Remove()
+                    End If
+                End If
+            End If
+        Next
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
